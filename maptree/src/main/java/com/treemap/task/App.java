3488b4dce878364f7929a79eb7ed1f2e03ddbab2
sanjumahi <gaddamsanjai545@gmail.com>
2023-12-19 12:49:26 +0000

    first commit

---
+++ maptree/src/main/java/com/treemap/task/App.java
@@ -0,0 +1,13 @@
+package com.treemap.task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maptree/src/main/java/com/treemap/task/Treemap.java
@@ -0,0 +1,16 @@
+package com.treemap.task;
+import java.util.*;
+//import Treemap.xam;
+public class Treemap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Map <String,Integer> td = new TreeMap<String,Integer>();
+    td.put("Prabhas",10);
+    td.put("Dhoni", 07);
+    td.put("Sanjay", 37);
+    td.put("Siddu", 50);
+    
+   System.out.println(td);
+	}
+}
